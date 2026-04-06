fb19dc47f85be4b873d98918020a820b46001cf2
hasanelfalakiy <andihasanelfalakiy@gmail.com>
2026-04-06 14:31:05 +0700

    fix firebase analitics

--- common/src/dev/java/andihasan7/kartikaide/common/Analytics.kt
+++ common/src/dev/java/andihasan7/kartikaide/common/Analytics.kt
@@ -5,31 +5,27 @@
  * You should have received a copy of the GNU General Public License along with Cosmic IDE. If not, see <https://www.gnu.org/licenses/>.
  */
 
-/*
- * This file is part of Cosmic IDE.
- * Cosmic IDE is a free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
- * Cosmic IDE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License along with Cosmic IDE. If not, see <https://www.gnu.org/licenses/>.
- */
-
 package andihasan7.kartikaide.common
 
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import com.google.firebase.FirebaseApp
-import com.google.firebase.analytics.ktx.analytics
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.analytics.FirebaseAnalytics
 
 object Analytics {
 
     private var isAnalyticsCollectionEnabled = true
-
-    private val analytics by lazy { Firebase.analytics }
+    private var firebaseAnalytics: Any? = null
 
     @JvmStatic
     fun init(context: Context) {
-        FirebaseApp.initializeApp(context)
+        try {
+            FirebaseApp.initializeApp(context)
+            firebaseAnalytics = FirebaseAnalytics.getInstance(context)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
     fun logEvent(event: String, value: Any) {
@@ -37,9 +33,10 @@ object Analytics {
         Log.d("Analytics", "Logging event: $event")
 
         try {
-            analytics.logEvent(event, Bundle().apply {
+            val bundle = Bundle().apply {
                 putString("value", value.toString())
-            })
+            }
+            (firebaseAnalytics as? FirebaseAnalytics)?.logEvent(event, bundle)
         } catch (e: Exception) {
             e.printStackTrace()
         }
@@ -50,7 +47,7 @@ object Analytics {
         Log.d("Analytics", "Logging event: $event")
 
         try {
-            analytics.logEvent(event, bundle)
+            (firebaseAnalytics as? FirebaseAnalytics)?.logEvent(event, bundle)
         } catch (e: Exception) {
             e.printStackTrace()
         }
@@ -73,8 +70,9 @@ object Analytics {
 
     @JvmStatic
     fun setAnalyticsCollectionEnabled(enabled: Boolean) {
+        isAnalyticsCollectionEnabled = enabled
         try {
-            isAnalyticsCollectionEnabled = enabled
+            (firebaseAnalytics as? FirebaseAnalytics)?.setAnalyticsCollectionEnabled(enabled)
         } catch (e: Exception) {
             e.printStackTrace()
         }
